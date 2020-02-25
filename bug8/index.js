772f34e5de5c0507b6c72109af64a7a051653a11
zgard <zfgardner@gmail.com>
2020-02-25 18:33:22 -0500

    Debugged file

--- bug8/index.js
+++ bug8/index.js
@@ -7,6 +7,7 @@ function renderTransactions(transactions) {
 	var finalHTML = '<div class="buffer">TRANSACTIONS</div>';
 
 	var transactionsHTML = transactions.map(function (transaction) {
+		debugger;
 		var transactionHTML = `
 		<div class="transaction">
 			<div class="name">${transaction.name}</div>
@@ -19,7 +20,6 @@ function renderTransactions(transactions) {
 	});
 
 	finalHTML += transactionsHTML.join('');
-
 	return finalHTML;
 }
 
@@ -31,7 +31,7 @@ function renderTransactions(transactions) {
 document.addEventListener("DOMContentLoaded", function () {
 	document.getElementById('transactions').innerHTML = renderTransactions(fullTransactionData);
 
-	document.getElementById('search-input').addEventListener('change', function (e) {
+	document.getElementById('search-input').addEventListener('input', function (e) {
 		var searchString = e.target.value.toLowerCase();
 		var filteredData = fullTransactionData.filter(function (transaction) {
 			var foundInName = transaction.name.toLowerCase().indexOf(searchString) > -1;
